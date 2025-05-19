090724c4abe451ff0b5134a9b85783216476c128
nekiynekta <manyakafenil@gmail.com>
2025-05-19 19:12:03 +0300

    APP Version 3.9 -- Додано у headers "Content-Type": "application/json"

--- netlify/functions/mono-callback.js
+++ netlify/functions/mono-callback.js
@@ -2,7 +2,8 @@ export async function handler(event, context) {
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "POST, OPTIONS"
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Content-Type": "application/json"
   };
 
   if (event.httpMethod === "OPTIONS") {
